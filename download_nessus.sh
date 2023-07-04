9c8d6ab50ad746c9db4f0ae5520b4fc01bbd1d0f
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-07-04 14:40:16 +0530

    Create download_nessus.sh

---
+++ download_nessus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to download and start Nessus
+
+# Download Nessus package
+curl --request GET \
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.5.3-ubuntu1404_amd64.deb' \
+  --output 'Nessus-10.5.3-ubuntu1404_amd64.deb'
+
+# Install Nessus package
+sudo dpkg -i Nessus-10.5.3-ubuntu1404_amd64.deb
+
+# Start Nessus service
+sudo /etc/init.d/nessusd start
+
+echo "Nessus has been downloaded and started successfully."
